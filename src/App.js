4ae04d9ede67bddd8a602e8e8cd40f21fe3591f7
jeffnguyener <jeffnguyen.83@gmail.com>
2019-12-16 14:50:15 -0600

    API test works

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
+  const APP_ID = "9186a068";
+  const APP_KEY = "2b40af8c45e601045cbee4281405bdbd";
 
-  const APP_ID = '9186a068';
-  const APP_KEY = '2b40af8c45e601045cbee4281405bdbd';
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    console.log(data)
+  }
 
-  return(
+  return (
     <div className="App">
       <form className="search-form">
-        <input className="search-bar" type="text"/>
-        <button className="search-button" type='submit'>Search</button>
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default App;
