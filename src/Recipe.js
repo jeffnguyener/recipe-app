af62200617c38be8b617ee31c2dcb2314ec91613
jeffnguyener <jeffnguyen.83@gmail.com>
2019-12-17 23:06:57 -0600

    Added h3 to label

--- src/Recipe.js
+++ src/Recipe.js
@@ -6,11 +6,13 @@ const Recipe = ({ title, calories, image, ingredients}) => {
   return (
     <div className="recipe">
       <h1>{title}</h1>
+      <h3>Ingredients</h3>
       <ol>
         {ingredients.map(ingredient => (
           <li>{ingredient.text}</li>
         ))}
       </ol>
+      <h3>Calories</h3>
       <p>{calories}</p>
       <img className="image" src={image} alt="" />
     </div>
